c85cf74ebd4a87b7b2a6e8b01ca43083f02c6d56
jason <jason@jasons-MacBook-Pro.local>
2019-08-06 13:46:36 +0800

    deploy nodejs

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ var app = express()
 
 app.use('/', express.static(__dirname + '/'));
 
-app.listen(8000, function () {
+/*
+var path = require("path");
+app.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/index.html'));
+});*/
+
+app.listen(process.env.PORT || 8000, function () {
   console.log('Example app listening on port 8000!')
 })
